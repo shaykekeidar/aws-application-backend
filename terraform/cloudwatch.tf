2cee60dccbc4c35bb0d416ff9ed3960b60f19a9d
Shay Keidar <shaykekeidar@gmail.com>
2026-07-29 10:20:33 +0300

    moved the entire terraform folder to other repo

--- terraform/cloudwatch.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_cloudwatch_log_group" "backend" {
-  name              = "/ecs/${local.name_prefix}-backend"
-  retention_in_days = 7
-
-  tags = {
-    Name = "${local.name_prefix}-backend-logs"
-  }
-}
