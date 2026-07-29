fac6a186eb4017fdca70c9a4ad6b3312db81b4cd
Shay Keidar <shaykekeidar@gmail.com>
2026-07-29 11:39:24 +0300

    fixed

--- test-backend.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$terraformDirectory = Join-Path $PSScriptRoot "terraform"
-$alb = terraform -chdir="$terraformDirectory" output -raw alb_dns_name
-
-Write-Host "Testing backend through ALB: http://$alb/api/hello"
-
-Invoke-RestMethod "http://$alb/api/hello"
