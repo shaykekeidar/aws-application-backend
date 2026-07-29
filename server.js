b2e103f6eee2fe019211b14e9314d50822969bab
Shay Keidar <shaykekeidar@gmail.com>
2026-07-29 12:42:52 +0300

    fixed message

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get("/api/status", (req, res) => {
 
 app.get("/api/hello", (req, res) => {
   res.json({
-    message: "Hello from GitHub Actions on Amazon ECS Fargate.",
+    message: "Hello cloud-modules.",
   });
 });
 
